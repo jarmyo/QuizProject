a49ac96aed48d7d25eafb98f7dfc0cbc9e864654
Julian Augusto Ruiz Martinez <julian.ruiz@ti.com>
2022-03-10 10:56:57 -0600

    dashboard start

---
+++ Pages/Dashboard.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace QuizProject.Pages
+{
+    public class DashboardyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+        private readonly UserManager<IdentityUser> _userManager;
+        public DashboardyModel(ILogger<PrivacyModel> logger,
+              UserManager<IdentityUser> userManager)
+        {
+            _logger = logger;
+        }
+
+        public string TeamName { get; set; }
+
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            return Page();
+        }
+    }
+}
