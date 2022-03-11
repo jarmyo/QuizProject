0816638bfb947ef85db0542520b5a1bfa3253f17
Julian Augusto Ruiz Martinez <jarm.yo@gmail.com>
2022-03-11 10:09:25 -0600

    reorder project structure

---
+++ src/Pages/Resulter.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace QuizProject.Pages
+{
+    public class ResulterModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly QuizContext _context;
+        public ResulterModel(ILogger<PrivacyModel> logger,
+              UserManager<IdentityUser> userManager,
+              QuizContext context)
+        {
+            _logger = logger;
+            _userManager = userManager;
+            _context = context;
+        }
+        public IActionResult OnPost([FromBody] ResultResponseModel response)
+        {
+            //Get the current Team.
+
+            var answer = response.Response;
+            var validAnswers = _context.Answers.Where(a => a.IdQuestion == response.Question).ToList();
+            var check = Verificator.Check(validAnswers, answer);
+            if (check.IsCorrect)
+            {
+                //Move
+                return new JsonResult(new { Result = "Correct", Score = check.Score });
+            }
+            return new JsonResult(new { Result = "Incorrect", Score = check.Score });
+        }
+
+        public class ResultResponseModel
+        {
+            public string Question { get; set; }
+            public string Response { get; set; }
+        }
+    }
+}
---
+++ tests/VerificatorTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QuizProject;
+using QuizProject.Databases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizProject.Pages.ResulterModel.Tests
+{
+    [TestClass()]
+    public class VerificatorTests
+    {
+        //TODO: Create more test cases
+        public static List<Answer> CorrectAnswers { get; set; } = new List<Answer>()
+        {
+            new Answer() { Id="x", IdQuestion="y", Text="Marathon" }
+        };
+
+        [TestMethod()]
+        public void CheckTest()
+        {
+            var data = Verificator.Check(CorrectAnswers, "MARATHON");
+            Assert.IsTrue(data.IsCorrect);            
+        }
+    }
+}
