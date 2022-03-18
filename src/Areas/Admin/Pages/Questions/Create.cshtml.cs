acd3316e9769e14a6d90b7e21415af3ad36ad85a
Julian Augusto Ruiz Martinez <jarm.yo@gmail.com>
2022-03-18 12:01:21 -0600

    CRUD questions update

--- src/Areas/Admin/Pages/Questions/Create.cshtml.cs
+++ src/Areas/Admin/Pages/Questions/Create.cshtml.cs
@@ -12,29 +12,24 @@ namespace QuizProject.Areas.Admin.Pages.Questions
     public class CreateModel : PageModel
     {
         private readonly QuizContext _context;
-
         public CreateModel(QuizContext context)
         {
             _context = context;
         }
-
         public IActionResult OnGet()
         {
-        ViewData["IdCategory"] = new SelectList(_context.Categories, "Id", "Id");
+            ViewData["IdCategory"] = new SelectList(_context.Categories, "Id", "Title");
             return Page();
         }
-
         [BindProperty]
         public Question Question { get; set; }
-
-        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
         public async Task<IActionResult> OnPostAsync()
         {
             if (!ModelState.IsValid)
             {
                 return Page();
             }
-
+            Question.Id = Guid.NewGuid().ToString();
             _context.Questions.Add(Question);
             await _context.SaveChangesAsync();
 
--- src/Areas/Admin/Pages/Questions/Edit.cshtml.cs
+++ src/Areas/Admin/Pages/Questions/Edit.cshtml.cs
@@ -1,12 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using QuizProject.Databases;
 
 namespace QuizProject.Areas.Admin.Pages.Questions
 {
@@ -30,7 +25,9 @@ namespace QuizProject.Areas.Admin.Pages.Questions
             }
 
             Question = await _context.Questions
-                .Include(q => q.IdCategoryNavigation).FirstOrDefaultAsync(m => m.Id == id);
+                .Include(q => q.IdCategoryNavigation)
+                .Include(q=>q.Answers)
+                .FirstOrDefaultAsync(m => m.Id == id);
 
             if (Question == null)
             {
