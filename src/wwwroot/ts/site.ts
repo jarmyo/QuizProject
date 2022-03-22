6df28a62a3d5ac1fdf14169da6296914cc0a1b46
Julian Augusto Ruiz Martinez <jarm.yo@gmail.com>
2022-03-22 11:37:39 -0600

    move to typescript

---
+++ src/wwwroot/ts/site.ts
@@ -0,0 +1,23 @@
+﻿function PostAnswer(questionId: string): void {
+    var AnswerTextField = <HTMLInputElement>document.getElementById("TeamAnser-" + questionId);
+    var token = <HTMLInputElement>  document.getElementsByName("__RequestVerificationToken")[0]
+    fetch('/Resulter',
+        {
+            method: "POST",
+            body: JSON.stringify({
+                Question: questionId,
+                Response: AnswerTextField.value
+            }),
+            headers: {
+                RequestVerificationToken: token.value,
+                'Content-Type': 'application/json',
+                Accept: 'application/json',
+            }
+        }).then(function (result) {
+            return result.json();
+        }).then(function (r2) {
+            console.log(r2);
+            //TODO: here, notify if success or fail.
+        }
+        );
+}
